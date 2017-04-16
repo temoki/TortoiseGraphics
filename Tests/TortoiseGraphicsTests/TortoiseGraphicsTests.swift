d5df2f12abfb13f40a233c178c3d7e21aa413c8f
temoki <temoki@gmail.com>
2017-04-16 23:10:20 +0900

    add test example code

--- Tests/TortoiseGraphicsTests/TortoiseGraphicsTests.swift
+++ Tests/TortoiseGraphicsTests/TortoiseGraphicsTests.swift
@@ -1,15 +1,50 @@
 import XCTest
 @testable import TortoiseGraphics
+import CoreGraphics
 
 class TortoiseGraphicsTests: XCTestCase {
+    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(TortoiseGraphics().text, "Hello, World!")
-    }
+        let canvas = Canvas(width: 300, height: 300)
+        
+        let color = "color"
+        canvas.🐢
+            //.clearScreen()
+            .make(color, 0)
+            .repeat(12) { $0
+                .setPenWidth(2)
+                .right(15)
+                .repeat(6) { $0
+                    .setPenColor { $0.penColor + 1 }
+                    .forward(50)
+                    .right(60)
+                }
+                .setPenWidth(1)
+                .right(15)
+                .repeat(6) { $0
+                    .make(color) { $0[color] + 1 }
+                    .setPenColor({ $0[color] })
+                    .forward(20)
+                    .right(60)
+                }
+            }
+            .done()
+        canvas.draw()
 
+        let image = canvas.rendered
+        XCTAssertNotNil(image)
+
+        let path = NSHomeDirectory().appending("/Desktop/TortoiseGraphicsExample.png")
+        let url = URL(fileURLWithPath: path)
+        let destination = CGImageDestinationCreateWithURL(url as CFURL, kUTTypePNG, 1, nil)
+        XCTAssertNotNil(destination)
+        
+        CGImageDestinationAddImage(destination!, image!, nil)
+        CGImageDestinationFinalize(destination!)
+    }
 
     static var allTests = [
         ("testExample", testExample),
     ]
+
 }
