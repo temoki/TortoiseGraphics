07cfff73a5ff67cb778b08a070e54f5a26e25007
temoki <temoki@gmail.com>
2017-05-01 23:20:50 +0900

    add cocoapods installation test for iOS

---
+++ InstallationTests/Cocoapods/Cocoapods-iOS/Cocoapods-iOS/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Cocoapods-iOS
+//
+//  Created by temoki on 2017/05/01.
+//  Copyright © 2017年 temoki. All rights reserved.
+//
+
+import UIKit
+import TortoiseGraphics
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let view = CanvasView(canvasSize: self.view.bounds.size)
+        self.view.addSubview(view)
+        view.canvas.🐢
+            .repeat(36) { $0
+                .repeat(3) { $0
+                    .right(120)
+                    .forward(100)
+                }
+                .right(10)
+        }
+        view.animate(atTimeInterval: 0.1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
