3519062089322de919619a807c6d42469f90f1dc
temoki <temoki@gmail.com>
2017-05-06 01:00:35 +0900

    add PlaygroundBook generator

---
+++ PlaygroundBook/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Directories
+SELF_DIR=$(cd $(dirname $0);pwd)
+
+# Make book with template
+BOOK_DIR=$SELF_DIR/TortoisePlayground.playgroundbook
+if [ -e $BOOK_DIR ]; then
+  rm -rf $BOOK_DIR
+fi
+mkdir $BOOK_DIR
+cp -rf $SELF_DIR/template/* $BOOK_DIR
+
+# Copy sources
+echo "Copy sources..."
+FROM_SOURCES_DIR=$SELF_DIR/../Sources
+TO_SOURCES_DIR=$BOOK_DIR/Contents/Sources
+rm -f $TO_SOURCES_DIR/*
+cp $FROM_SOURCES_DIR/* $TO_SOURCES_DIR
+
+# Copy Contents.swift
+echo "Copy Contents.swift" 
+CONTENTS_SWIFT=$SELF_DIR/Tortoise.playground/Pages/TortoisePlayground.xcplaygroundpage/Contents.swift
+cp -f $CONTENTS_SWIFT $BOOK_DIR/Contents/Chapters/Main.playgroundchapter/Pages/TortoisePlayground.playgroundpage
+
+echo "Done."
