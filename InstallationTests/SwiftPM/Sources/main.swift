2e781ae83ddcd0eec88563eefca29eddce205a4b
temoki <temoki@gmail.com>
2017-06-12 00:20:46 +0900

    Fix SwiftPM installation test code.

--- InstallationTests/SwiftPM/Sources/main.swift
+++ InstallationTests/SwiftPM/Sources/main.swift
@@ -1,28 +1,5 @@
 import Foundation
-import CoreGraphics
 import TortoiseGraphics
-
-let canvas = Canvas(size: CGSize(width: 300, height: 300))
-
-canvas.🐢
-    .penUp()
-    .forward(50)
-    .right(90)
-    .forward(50)
-    .penDown()
-    .repeat(4) { $0
-        .right(90)
-        .forward(100)
-    }
-
-guard let image = canvas.draw() else {
-    fatalError()
-}
-
-let url = URL(fileURLWithPath: "./output.png")
-guard let destination = CGImageDestinationCreateWithURL(url as CFURL, kUTTypePNG, 1, nil) else {
-    fatalError()
-}
-
-CGImageDestinationAddImage(destination, image, nil)
-CGImageDestinationFinalize(destination)
+let 🐢 = Tortoise()
+🐢.fd(100)
+🐢.writePNG(of: CGSize(width: 300, height: 300), to: URL(fileURLWithPath: "./output.png"))
