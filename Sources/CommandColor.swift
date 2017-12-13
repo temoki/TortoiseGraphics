6814f9e8281ddfb4da96485aef4b319bc960ee36
temoki <temoki@gmail.com>
2017-12-14 00:08:58 +0900

    Add "Filling" commands

---
+++ Sources/CommandColor.swift
@@ -0,0 +1,45 @@
+import CoreGraphics
+
+class CommandColor: Command {
+
+    enum ColorType {
+        case pen
+        case fill
+    }
+
+    private let type: ColorType
+    private let color: CGColor
+
+    init(_ type: ColorType, color: Color) {
+        self.type = type
+        self.color = color.cgColor
+    }
+
+    init(_ type: ColorType, red: CGFloat, green: CGFloat, blue: CGFloat) {
+        self.type = type
+        self.color = CGColor.rgb(red, green, blue)
+    }
+
+    func test(in state: State) -> State {
+        var newState = state
+        switch type {
+        case .pen:
+            newState.penColor = color
+        case .fill:
+            newState.fillColor = color
+        }
+        return newState
+    }
+
+    func exexute(in state: State, with context: CGContext) -> State {
+        let newState = test(in: state)
+        switch type {
+        case .pen:
+            context.setStrokeColor(newState.penColor)
+        case .fill:
+            context.setFillColor(newState.fillColor)
+        }
+        return newState
+    }
+
+}
---
+++ Sources/CommandFill.swift
@@ -0,0 +1,43 @@
+import CoreGraphics
+
+class CommandFill: Command {
+
+    enum Operation {
+        case begin
+        case end
+    }
+
+    private let operation: Operation
+
+    init(_ operation: Operation) {
+        self.operation = operation
+    }
+
+    func test(in state: State) -> State {
+        var newState = state
+        switch operation {
+        case .begin:
+            newState.fillPath = CGMutablePath()
+        case .end:
+            newState.fillPath = nil
+        }
+        return newState
+    }
+
+    func exexute(in state: State, with context: CGContext) -> State {
+        var newState = state
+        switch operation {
+        case .begin:
+            newState.fillPath = CGMutablePath()
+            newState.fillPath?.move(to: newState.position)
+        case .end:
+            if let fillPath = newState.fillPath {
+                context.addPath(fillPath)
+                context.fillPath(using: .evenOdd)
+                newState.fillPath = nil
+            }
+        }
+        return newState
+    }
+
+}
