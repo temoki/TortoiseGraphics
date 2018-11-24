2d776841e869fea31478cd5fd96f1076f6b676ca
Russell Gordon <rgordon@lcs.on.ca>
2018-11-24 15:43:46 -0500

    Revert "Locking when reading/writing command histories"
    This reverts commit e7ca27f7d88a8d63ca8aecc6a6bb61d526b4abfd.

--- Sources/TortoiseCharmer.swift
+++ Sources/TortoiseCharmer.swift
@@ -5,9 +5,6 @@ class TortoiseCharmer {
     private(set) var tortoises: [Tortoise] = []
     private(set) var commandHistories: [(tortoiseTag: Int, commandIndex: Int)] = []
 
-    // ⭐️ Add lock object.
-    private let lock = NSLock()
-
     var commandedHandler: ((TortoiseCharmer) -> Void)?
 
     init(tortoiseCount: Int = 1) {
@@ -22,9 +19,6 @@ class TortoiseCharmer {
             let tortoise = Tortoise()
             tortoise.tag = index
             tortoise.commandedHandler = { [unowned self] (tortoise) in
-                // ⭐️ Lock while writing commandHistories.
-                defer { self.lock.unlock() }
-                self.lock.lock()
                 self.commandHistories.append((tortoiseTag: tortoise.tag,
                                               commandIndex: tortoise.commands.count - 1))
                 self.commandedHandler?(self)
@@ -35,10 +29,6 @@ class TortoiseCharmer {
 
     @discardableResult
     func charm(with context: GraphicsContext, toFrame index: Int?) -> Int {
-        // ⭐️ Lock while reading commandHistories.
-        defer { self.lock.unlock() }
-        self.lock.lock()
-
         let endIndex = commandHistories.count - 1
         let toIndex = min(max((index ?? endIndex), 0), endIndex)
 
@@ -49,11 +39,24 @@ class TortoiseCharmer {
             states[index].canvasSize = context.size
         }
 
+        // Make a copy of the array of commands for each Tortoise
+        var commandsToRun = [ Int: [Command] ]()
+        for (index, tortoise) in tortoises.enumerated() {
+            commandsToRun[index] = tortoise.commands
+        }
+
+        // Now iterate over all the commands in the command history and run
+        // using the copy of commands for each tortoise (saved a moment ago)
         for (index, history) in commandHistories.enumerated() where index <= toIndex {
-            states[history.tortoiseTag] =
-                tortoises[history.tortoiseTag]
-                    .commands[history.commandIndex]
+
+            // Get commands out of the dictionary for the current tortoise
+            if let commands = commandsToRun[history.tortoiseTag] {
+
+                // Actually run the command at the given index
+                states[history.tortoiseTag] = commands[history.commandIndex]
                     .exexute(in: states[history.tortoiseTag], with: context.cgContext)
+
+            }
         }
 
         for index in 0..<tortoises.count {
