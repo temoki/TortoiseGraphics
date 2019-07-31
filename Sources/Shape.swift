d20e4c7a60b6137df1ce1270f63fe1e32cfc010f
Tomoki Kobayashi <temoki@gmail.com>
2019-07-31 22:14:34 +0900

    Path の定義も Internal に隠す

--- Sources/Shape.swift
+++ Sources/Shape.swift
@@ -7,51 +7,27 @@ public struct Shape: CustomStringConvertible {
     public var isVisible: Bool = true
 
     public static var arrow: Shape {
-        return Shape("arrow", path: .points([
-            Vec2D( 0.00, 0.00),
-            Vec2D( 0.50, -0.70),
-            Vec2D( 0.00, -0.70),
-            Vec2D( 0.00, -1.00),
-            Vec2D( 0.00, -0.70),
-            Vec2D(-0.50, -0.70)
-            ]))
+        return Shape("arrow", path: Path.arrowPath())
     }
 
     public static var tortoise: Shape {
-        let side1: [Vec2D] = [
-            Vec2D( 0.00, 0.75),
-            Vec2D( 0.12, 0.63),
-            Vec2D( 0.06, 0.39),
-            Vec2D( 0.24, 0.18),
-            Vec2D( 0.42, 0.30),
-            Vec2D( 0.54, 0.24),
-            Vec2D( 0.36, 0.06),
-            Vec2D( 0.42, -0.18),
-            Vec2D( 0.30, -0.42),
-            Vec2D( 0.48, -0.60),
-            Vec2D( 0.36, -0.75),
-            Vec2D( 0.24, -0.54),
-            Vec2D( 0.00, -0.66)
-        ]
-        let side2 = side1.map { Vec2D(-$0.x, $0.y) }.reversed()
-        return Shape("tortoise", path: .points(side1 + side2))
+        return Shape("tortoise", path: Path.tortoisePath())
     }
 
     public static var circle: Shape {
-        return Shape("circle", path: .ellipse(origin: Vec2D(-0.5, -0.5), size: Vec2D(1, 1)))
+        return Shape("circle", path: Path.circlePath())
     }
 
     public static var square: Shape {
-        return Shape("square", path: .rect(origin: Vec2D(-0.5, -0.5), size: Vec2D(1, 1)))
+        return Shape("square", path: Path.squarePath())
     }
 
     public static var triangle: Shape {
-        let y = cos(Double.pi / 6)
-        return Shape("triangle", path: .points([Vec2D(0, 0), Vec2D(0.5, -y), Vec2D(-0.5, -y)]))
+        return Shape("triangle", path: Path.trianglePath())
     }
 
     public static var classic: Shape {
-        return Shape("classic", path: .points([Vec2D(0, 0), Vec2D(0.5, -1), Vec2D(0, -0.75), Vec2D(-0.5, -1)]))
+        return Shape("classic", path: Path.classicPath())
     }
 
     // MARK: - CustomStringConvertible
@@ -62,15 +38,15 @@ public struct Shape: CustomStringConvertible {
 
     // MARK: - Internal
 
-    enum ShapePath {
+    enum Path {
         case points([Vec2D])
         case ellipse(origin: Vec2D, size: Vec2D)
         case rect(origin: Vec2D, size: Vec2D)
     }
 
-    let path: ShapePath
+    let path: Path
 
-    init(_ name: String, path: ShapePath) {
+    init(_ name: String, path: Path) {
         self.name = name
         self.path = path
     }
