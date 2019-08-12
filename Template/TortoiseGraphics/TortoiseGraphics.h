32c447d8105afb14769bc8bcde46cde66296cbbc
Tomoki Kobayashi <temoki@gmail.com>
2019-08-12 16:15:07 +0900

    TortoiseGraphics.framewokr 用のターゲットと Xcode Playground 追加

---
+++ Template/TortoiseGraphics/TortoiseGraphics.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for TortoiseGraphics.
+FOUNDATION_EXPORT double TortoiseGraphicsVersionNumber;
+
+//! Project version string for TortoiseGraphics.
+FOUNDATION_EXPORT const unsigned char TortoiseGraphicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TortoiseGraphics/PublicHeader.h>
+
+
