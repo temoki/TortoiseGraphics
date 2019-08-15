a393c3a846c3d77b8d59c2bf78a5bddf67ad8ac6
Tomoki Kobayashi <temoki@gmail.com>
2019-08-15 10:38:18 +0900

    Carthage と Cocoapods のインストールテスト追加

---
+++ InstallationTests/Cocoapods/Cocoapods-iOS/Cocoapods-iOS/CanvasView.swift
@@ -0,0 +1,43 @@
+//
+//  CanvasView.swift
+//  Cocoapods-iOS
+//
+//  Created by temoki on 2017/12/17.
+//  Copyright © 2017 temoki. All rights reserved.
+//
+
+import UIKit
+import TortoiseGraphics
+
+class CanvasView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        let canvas = ImageCanvas(size: Vec2D(Double(bounds.size.width),
+                                             Double(bounds.size.height)),
+                                 scale: Double(UIScreen.main.scale))
+        let 🐢 = Tortoise()
+        canvas.add(🐢)
+
+        🐢.penColor(.red)
+        🐢.fillColor(.yellow)
+        
+        🐢.penUp()
+        🐢.back(100)
+        🐢.penDown()
+        
+        // Turtle Star!
+        🐢.beginFill()
+        🐢.repeat(36) {
+            🐢.forward(200)
+            🐢.left(170)
+        }
+        🐢.endFill()
+        
+        if let image = canvas.cgImage {
+            context.draw(image, in: bounds)
+        }
+    }
+
+}
---
+++ InstallationTests/README.md
@@ -0,0 +1,19 @@
+# Installation Test
+
+Test the installation using each package manager in the `develop` branch.
+
+## Carthage
+
+```shell
+cd ./Carthage
+carthage update
+```
+
+## Cocoapods
+
+```shell
+cd ./Cocoapods/Cocoapods-iOS
+pod update
+open Cocoapods-iOS.xcworkspace
+# -> Build by Xcode
+```
